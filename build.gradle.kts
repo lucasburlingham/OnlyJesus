e07df5305fad6ba967582568ce94fa56fe2314c3
Lucas Burlingham <lucas@lucasburlingham.me>
2026-06-04 09:59:06 -0400

    refactor: comment out unused APK output configuration in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,12 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+
+// android.applicationVariants.all { variant ->
+
+//     def outputName = "OnlyJesus-latest.apk"
+// 	def path_to_filename = "${buildDir}/outputs/apk/${variant.name}/${outputName}"
+
+//     variant.outputFile = file(path_to_filename)
+
+// }
